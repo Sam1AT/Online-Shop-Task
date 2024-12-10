c4f8dbb04a4dfbf2eb2731031e3daa98a7426a9a
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-10 23:51:32 +0330

    add: added user custom model and set it as default user

--- settings/settings.py
+++ settings/settings.py
@@ -112,6 +112,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 

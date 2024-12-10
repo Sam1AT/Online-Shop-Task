f989932322d7dfb0d9deae8b606d1fe6b6f68981
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 00:01:18 +0330

    feat: implimented registration feature

---
+++ accounts/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+from accounts.models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+    )
+    password_confirm = serializers.CharField(
+        write_only=True,
+        required=True,
+    )
+
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'password', 'password_confirm']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.ValidationError({"password": "Passwords do not match"})
+        return attrs
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(
+            username=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user

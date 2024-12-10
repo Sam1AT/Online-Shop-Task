0416ba6a052d9b88fd761bc7f8e1e5ba81e88bd3
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 00:28:30 +0330

    added auth end point

--- settings/urls.py
+++ settings/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/token/', include('accounts.urls')),
+    path('api/auth/', include('accounts.urls')),
 
 ]

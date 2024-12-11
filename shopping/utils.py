55b339c497953ea7cb5bf75b9b01f9cba6f3952f
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 16:46:47 +0330

    feat: making the query a transaction

--- shopping/utils.py
+++ shopping/utils.py
@@ -6,10 +6,10 @@ from shopping.models import (
 
 
 def create_cart_in_db(cart, user_id):
-    cart = Cart.objects.create(user_id=user_id)
+    cart_obj = Cart.objects.create(user_id=user_id)
 
     for product_id, quantity in cart.items():
         cart_item = CartItem.objects.create(product_id=product_id,
                                             quantity=quantity,
-                                            cart=cart)
-    cart.update_cart_price()
+                                            cart=cart_obj)
+    cart_obj.update_cart_price()

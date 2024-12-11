d9711eb3ef2d87f2d756d53bfb5bcd10b36190f6
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 10:50:47 +0330

    init: initiated settings and confs of redis

--- settings/settings.py
+++ settings/settings.py
@@ -143,3 +143,12 @@ SIMPLE_JWT = {
     'ALGORITHM': 'HS256',
     'SIGNING_KEY': SECRET_KEY,
 }
+
+# Redis configs
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
+
+REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+

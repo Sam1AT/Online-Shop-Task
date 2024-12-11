95d8f4d92171f9b3949a5e7ba525a7d4184af86e
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 21:35:40 +0330

    add: added the base of CustomTotalCart end point

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+
+
+
+class GetCustomerTotalCart(APIView):
+    def get(self, request):
+        pass
+
+

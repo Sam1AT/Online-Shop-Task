ad7e66d56445344dc5cad45d0b5bc051a526ce03
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 19:52:20 +0330

    bugfix: deleted the function calling

--- shopping/apps.py
+++ shopping/apps.py
@@ -6,6 +6,4 @@ class ShoppingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'shopping'
 
-    def ready(self):
-        from .tasks import start_expiry_listener
-        start_expiry_listener()
+

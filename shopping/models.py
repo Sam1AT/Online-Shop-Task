3e53b7e7de1a8972eaf735189ff38850a288b0ab
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-11 11:55:49 +0330

    add: product model and migrated

--- shopping/models.py
+++ shopping/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Product(Base):
+    name = models.CharField(max_length=255)
+    price = models.IntegerField()

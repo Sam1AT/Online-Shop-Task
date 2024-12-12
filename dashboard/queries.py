87db8ca90339a713f0569b640283a4433a45048b
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-12 12:15:52 +0330

    hotfix: change the group by part of the query

--- dashboard/queries.py
+++ dashboard/queries.py
@@ -7,7 +7,7 @@ def cart_price_by_date_user():
     cart_data = (
         Cart.objects.annotate(date=Cast(TruncDate("created_at"), CharField()))
         .annotate(name=Concat(F('user__first_name'), Value(' '),  F('user__last_name')))
-        .values('name', 'date')
+        .values('user__email', 'date', 'name')
         .annotate(price=Sum(F('price')))
         .order_by('date', '-price')
     )

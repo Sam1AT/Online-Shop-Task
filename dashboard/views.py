f30379be028e91eb36ad9ff2344180d6becc1998
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-12 11:50:22 +0330

    feat: added pagination base

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 
 from dashboard.queries import cart_price_by_date_user
 from dashboard.utils import parse_cart_list
+
+
 class GetCustomersTotalCart(APIView):
+    class CustomPagination(PageNumberPagination):
+        page_size = 10
+        page_size_query_param = 'page_size'
+
     def get(self, request):
         cart_date_user_list = parse_cart_list(cart_price_by_date_user())
 
-        return Response(cart_date_user_list)
+        paginator = self.CustomPagination()
+        paginated_data = paginator.paginate_queryset(cart_date_user_list, request)
+
+        return paginator.get_paginated_response(paginated_data)
 

2c4dd8dc42e3ddcdf6aa6b77c05674849b5fc1da
Sam Ahmadizadeh <alileon32@gmail.com>
2024-12-12 12:15:35 +0330

    edit: change the number of pagination

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,7 +9,7 @@ from dashboard.utils import parse_cart_list
 
 class GetCustomersTotalCart(APIView):
     class CustomPagination(PageNumberPagination):
-        page_size = 10
+        page_size = 1
         page_size_query_param = 'page_size'
 
     def get(self, request):
